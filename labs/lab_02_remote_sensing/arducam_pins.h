a5edf66edb3e60733a79283808bb8044d4a24671
jordanfirthussf <jordan.firth@afacademy.af.edu>
2024-02-10 12:46:53 -0700

    changed pin definitions to work with mkrzero or teensy 4.1

--- labs/lab_02_remote_sensing/arducam_pins.h
+++ labs/lab_02_remote_sensing/arducam_pins.h
@@ -1,11 +1,31 @@
 // arducam pin = arduino pin
 
-// pin 1 is left-most pin when lens is facing you
-const int CS  = 7; // arducam pin 1
-      // MOSI = MOSI (may be labelled COPI)
-      // MISO = MISO (may be labelled CIPO)
-      // SCLK = SCK
+
+#ifdef ARDUINO_TEENSY41 
+// if using teensy (green)
+const int CS  = 10; // arducam pin 1 to arduino pin 10 (CS)
+      // MOSI = 11 (MOSI) (may be labelled COPI)
+      // MISO = 12 (MISO) (may be labelled CIPO)
+      // SCLK = 13 (SCK)
       // GND  = GND (rail)
       // +5V  = Vcc (rail--3.3V may be OK for 2MP arducam)
-      // SDA  = SDA 
-      // SCL  = SCL 
+      // SDA  = 18 (SDA)
+      // SCL  = 19 (SCL) 
+
+#else
+
+//if using mkrzero (blue)
+const int CS  = 7; // arducam pin 1 to arduino pin 7 
+      // MOSI = 8 MOSI (may be labelled COPI)
+      // MISO = 10 MISO (may be labelled CIPO)
+      // SCLK = 9 SCK
+      // GND  = GND (rail)
+      // +5V  = Vcc (rail--3.3V may be OK for 2MP arducam)
+      // SDA  = 11 SDA 
+      // SCL  = 12 SCL 
+
+
+#endif
+
+
+
