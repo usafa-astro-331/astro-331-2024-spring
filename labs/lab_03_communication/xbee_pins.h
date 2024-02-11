f7d9a09ea9e18c53347fe653326a54478e12c6ed
jordanfirthussf <jordan.firth@afacademy.af.edu>
2024-02-10 17:54:48 -0700

    add pin connection list for using teensy

--- labs/lab_03_communication/xbee_pins.h
+++ labs/lab_03_communication/xbee_pins.h
@@ -1,8 +1,17 @@
-// xbee pin = arduino pin
-
 // NOTE: XBee explorer pin labels are located on the BOTTOM of the PCB
 // 
+//
+// if using MKR Zero
+// xbee pin = arduino pin
 //  GND   =   GND (ground rail)
 //  3.3V  =   Vcc (3.3 V rail)
-//  DOUT  =   13
-//  DIN   =   14
+//  DOUT  =   13 (RX)
+//  DIN   =   14 (TX) 
+//
+//
+// if using Teensy 4.1
+// xbee pin = arduino pin
+//  GND   =   GND (ground rail)
+//  3.3V  =   Vcc (3.3 V rail)
+//  DOUT  =   0 (RX)
+//  DIN   =   1 (TX)
